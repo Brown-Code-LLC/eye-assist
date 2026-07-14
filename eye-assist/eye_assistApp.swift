44e4b06ac0063ddc2bd0b2f2165cddd795b4b395
Kojo-Brown <100282824+Kojo-Brown@users.noreply.github.com>
2026-07-14 14:04:34 -0400

    Initial Commit

---
+++ eye-assist/eye_assistApp.swift
@@ -0,0 +1,17 @@
+//
+//  eye_assistApp.swift
+//  eye-assist
+//
+//  Created by Nicholas Darko Brown on 7/14/26.
+//
+
+import SwiftUI
+
+@main
+struct eye_assistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
